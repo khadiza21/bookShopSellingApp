f2566b639d43c3ef865a18bf3a98724788fde464
khadiza21 <bibikhadiza474@gmail.com>
2023-12-30 01:28:53 +0600

    book selling app create . Designing continue..... book details page updated

--- app/src/main/java/com/example/bookshopapp/bookDetails.java
+++ app/src/main/java/com/example/bookshopapp/bookDetails.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class testImage extends AppCompatActivity {
+public class bookDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_image);
+        setContentView(R.layout.activity_book_details);
     }
 }
