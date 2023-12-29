7e60b32bca0edf88b03b0731cbe7dcd19b3e7c6d
khadiza21 <bibikhadiza474@gmail.com>
2023-12-30 03:14:03 +0600

    book selling app create . Designing continue..... some page updated

---
+++ app/src/main/java/com/example/bookshopapp/booksList.java
@@ -0,0 +1,14 @@
+package com.example.bookshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class booksList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_books_list);
+    }
+}
