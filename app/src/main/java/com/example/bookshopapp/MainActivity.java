75c7d96626fef22d24739903bffa4e556372c923
khadiza21 <bibikhadiza474@gmail.com>
2024-01-06 19:58:41 +0600

    book selling app create . Designing continue..... some page updated

--- app/src/main/java/com/example/bookshopapp/MainActivity.java
+++ app/src/main/java/com/example/bookshopapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.bookshopapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnContinue,exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnContinue=(Button) findViewById(R.id.buttonContinue);
+        exit=(Button) findViewById(R.id.buttonExit);
+
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Welcome", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), login.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this, "Redirecting..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        exit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+
+            }
+        });
+
+
+
+
+
     }
 }
